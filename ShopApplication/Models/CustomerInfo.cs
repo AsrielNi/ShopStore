714581bd5d74edaae0a4b7e4d47998eefb5b8f70
AsrielNi <jasonplayer2010@gmail.com>
2024-06-14 17:28:15 +0800

    新增 Model - CustomerInfo 新增 Reference - TestZone -> ShopApplication 新增 Table - CustomerInfo in Shop.db

---
+++ ShopApplication/Models/CustomerInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopApplication.Models
+{
+    public class CustomerInfo
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public int? CustomerGender { get; set; } = null;
+        
+        public DateTime CustomerBirthday { get; set; }
+    }
+}
