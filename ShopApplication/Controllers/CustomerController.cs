a036ff6652d3a428866a0c69132292bc263cf9b4
AsrielNi <jasonplayer2010@gmail.com>
2024-06-18 02:51:04 +0800

    ShopApplication 修改檢視 Shared - _Layout.cshtml ShopApplication 新增控制器方法 CustomerController (Space) ShopApplication 新增檢視 Customer - Space.cshtml ShopApplication 修改檢視 Customer - Index.cshtml

--- ShopApplication/Controllers/CustomerController.cs
+++ ShopApplication/Controllers/CustomerController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopApplication.Data;
 
 namespace ShopApplication.Controllers
 {
     public class CustomerController : Controller
     {
+        public readonly ShopContext _shopContext;
+        public CustomerController(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Space(string customerName)
+        {
+            var result = await _shopContext.CustomerInfo.FirstOrDefaultAsync(m => m.CustomerName == customerName);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(result);
+            }
+            
         }
     }
 }
