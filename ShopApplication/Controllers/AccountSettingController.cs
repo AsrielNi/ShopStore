f86569056e8a926abe6c8bbbd0edde72bedaa223
AsrielNi <jasonplayer2010@gmail.com>
2024-07-02 18:10:39 +0800

    Hint: 建立帳戶設定頁面的空白MVC ShopApplication 新增空白控制器 AccountSettingController ShopApplication 新增空白控制器方法 AccountSettingController - (Index, PersonInfo) ShopApplication 新增預設檢視 AccountSetting - (Index.cshtml, PersonInfo.cshtml) P.S. 不新增模型 -> 參考LogInSystem的RegistrantsModel

---
+++ ShopApplication/Controllers/AccountSettingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApplication.Controllers
+{
+    public class AccountSettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PersonInfo()
+        {
+            return View();
+        }
+    }
+}
