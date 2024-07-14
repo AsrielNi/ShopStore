e9dc9eff8a1c2204128c10c7d162be076abbcbca
AsrielNi <jasonplayer2010@gmail.com>
2024-07-14 23:15:48 +0800

    Hint: 歸納所有LogInAPI相關的javascript LogInAPI 新增數個javascript方法 LogInAPI.js - (SignUp, LogIn) ShopApplication 修改檢視 Account - (LogIn.cshtml, SignUp.cshtml) README.md 新增內容

--- LogInAPI/wwwroot/js/LogInAPI.js
+++ LogInAPI/wwwroot/js/LogInAPI.js
@@ -1,4 +1,44 @@
 ﻿// 需要 jQuery
+// import { jQuery } from "./jquery-3.7.1";
+
+function SignUp(inputFormID, redirectURL) {
+    var formID = "#" + inputFormID
+    $(formID).submit(function (event) {
+        event.preventDefault();
+
+        var forms = $(this);
+
+        $.ajax({
+            type: document.getElementById(inputFormID).getAttribute("method"),
+            url: document.getElementById(inputFormID).getAttribute("action"),
+            data: forms.serialize(),
+            success: function (response) {
+                alert(response.message);
+                window.location = redirectURL;
+            }
+        })
+    });
+};
+
+function LogIn(inputFormID, redirectURL) {
+    var formID = "#" + inputFormID
+    $(formID).submit(function (event) {
+        event.preventDefault();
+
+        var forms = $(this);
+
+        $.ajax({
+            type: document.getElementById(inputFormID).getAttribute("method"),
+            url: document.getElementById(inputFormID).getAttribute("action"),
+            data: forms.serialize(),
+            success: function (response) {
+                var id = response.RegistrantID;
+                window.location = redirectURL + "?" + "id=" + id;
+            }
+        })
+    });
+};
+
 function LogOut(redirectURL) {
     $.ajax({
         type: "get",
