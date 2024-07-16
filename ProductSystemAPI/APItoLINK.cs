37aca318c1a407653294823879e4bf5ec6178c2e
AsrielNi <jasonplayer2010@gmail.com>
2024-07-16 18:00:04 +0800

    Hint: 修正一個錯誤 ProductSystemAPI 修改命名空間 APItoLINK.cs - LogInAPI -> ProductSystemAPI

--- ProductSystemAPI/APItoLINK.cs
+++ ProductSystemAPI/APItoLINK.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using ProductSystemAPI.Data;
 
-namespace LogInAPI
+namespace ProductSystemAPI
 {
     public class JsonSetting
     {
