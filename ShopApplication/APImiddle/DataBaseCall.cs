ac4ff2ffea3884cb6017aae68e61ae09981055c1
AsrielNi <jasonplayer2010@gmail.com>
2024-07-22 23:22:04 +0800

    Hint: 加入概念導引投影片、加入部分的註釋 新增 Project_Concept.pptx

--- ShopApplication/APImiddle/DataBaseCall.cs
+++ ShopApplication/APImiddle/DataBaseCall.cs
@@ -3,12 +3,18 @@ using ProductSystemAPI.Data;
 
 namespace ShopApplication.APImiddle
 {
+    // 管理WebAPI所使用的類別
+    // 目前僅管理Context
+    // 未來有可能會將方法改成static
     public class DataBaseCall
     {
+        // 引用LogInAPI的RegistrantContext
         public RegistrantContext FromRegistrantContext()
         {
             return LogInAPI.APItoLINK.GetContext();
         }
+
+        // 引用ProductSystemAPI的ProductContext
         public ProductContext FromProductContext()
         {
             return ProductSystemAPI.APItoLINK.GetContext();
